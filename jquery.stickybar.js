fae1c7d3edda77b0ce32047c83fea3d1338a275a
Nate Barr <nate@caffeinatelabs.com>
2014-05-08 20:46:01 -0400

    added second example, updated to allow both examples to work

--- jquery.stickybar.js
+++ jquery.stickybar.js
@@ -1,9 +1,24 @@
 (function($) {
-	$.fn.simpleStickyBar = function() {
+	$.fn.simpleStickyBar = function(options) {
 		return this.each(function() {
-			var stuck = false, $body = $('body'), $this = $(this), $parent, height, margin = 0, nowTop, offset;
-			offset = $this.offset() && $this.offset().top;
+			var stuck = false, $body = $('body'), $this = $(this), width, totalWidth, css, $parent, height, margin = 0, nowTop, offset;
+			var cssVal = function(property) {
+				return parseInt($this.css(property).replace(/[^\d]*/g, ''), 10) || 0;
+			};
+			options = options || {};
+			if (!options.hasOwnProperty('maintainHeight')) options.maintainHeight = false;
+			if (!options.hasOwnProperty('maintainWidth')) options.maintainWidth = false;
+			if (!options.hasOwnProperty('checkFullWidth')) options.checkFullWidth = false;
 
+			//get starting values
+			offset = $this.offset() && $this.offset().top;
+			if (options.maintainWidth) {
+				//set the current width of the div, if it is full width -> set to 100%
+				totalWidth = $this.width() + cssVal('margin-left') + cssVal('border-left-width') + cssVal('padding-left') + cssVal('padding-right') + cssVal('border-right-width') + cssVal('margin-right');
+				width = options.checkFullWidth && $this.parent().width() == totalWidth ? '100%' : $this.width();
+			}
+
+			//bind scroll event handler
 			$(window).on('scroll', function() {
 				//reset div, parent jquery objects if first time or if they no longer exists
 				$parent = $parent || $this.parent();
@@ -14,22 +29,26 @@
 
 				//adjust styling for stickybar according to position
 				if (!stuck && nowTop > offset) {
-					margin = parseInt($this.css('margin-top').replace(/[^\d]*/g, ''), 10) || 0;
-					$this.css({
+					margin = cssVal('margin-top');
+					css = {
 						'position': 'fixed',
 						'top': 0,
 						'z-index': 1000
-					});
+					};
+					if (options.maintainWidth) css.width = width;
+					$this.css(css);
 					$parent.css({
-						'margin-top': margin + height
+						'margin-top': options.maintainHeight ? margin + offset + height : margin + offset
 					});
 					stuck = true;
 				} else if (stuck && nowTop <= offset) {
-					$this.css({
+					css = {
 						'position': 'inherit',
 						'top': 'inherit',
 						'z-index': 'inherint'
-					});
+					};
+					if (options.maintainWidth) css.width = 'inherit';
+					$this.css(css);
 					$parent.css({
 						'margin-top': margin
 					});
