2a7cfca833d6670a7278a5c14f1e3292f06166ea
Nate Barr <nate@caffeinatelabs.com>
2014-05-21 16:43:09 -0400

    Add AMD compliance

--- jquery.stickybar.js
+++ jquery.stickybar.js
@@ -1,4 +1,12 @@
-(function($) {
+(function (factory) {
+if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module depending on jQuery.
+    define(['jquery'], factory);
+} else {
+    // No AMD. Register plugin with global jQuery object.
+    factory(jQuery);
+}
+}(function($) {
 	$.fn.simpleStickyBar = function(options) {
 		//function to add style rules to head
 		var addStyles = function() {
@@ -66,4 +74,4 @@
 			});
 		});
 	};
-}(jQuery));
+}));
